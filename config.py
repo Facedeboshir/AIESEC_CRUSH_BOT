4058010332dffe4b81bf7aa88c685bb3bb102df7
Facedeboshir <mustafindarhan0@gmail.com>
2023-06-25 14:07:55 +0600

    Add database connection

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "6270063717:AAGAHifgRrfPi8BXOWCu6zaZt9DPU7SQ07k"
+BOT_TOKEN = "6270063717:AAFsA4E72SmikeSpENTDJcnpI5_EIO-QT3w"
 APP_URL = "https://aiesec-9efaeaa86371.herokuapp.com/" + BOT_TOKEN
 DB_URI = "postgres://ttlukgbpjbzjsv:d0adcbc3520f6a6ba51b440ddde815095d41905fc947c1c5cd6596962d134728@ec2-34-242-199-141.eu-west-1.compute.amazonaws.com:5432/dc8u9hpggbagcb"
