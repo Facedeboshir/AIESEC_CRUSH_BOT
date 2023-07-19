7938665ef6a7b8425079669232ed1a919dbd5876
Darkhan Mustafin <220660@astanait.edu.kz>
2023-07-20 02:44:47 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "6270063717:AAFsA4E72SmikeSpENTDJcnpI5_EIO-QT3w"
+BOT_TOKEN = "6270063717:AAEeAnXDVDaafE2mvyY4-yMWebDdwPTihuA"
 APP_URL = "https://aiesec-9efaeaa86371.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://ttlukgbpjbzjsv:d0adcbc3520f6a6ba51b440ddde815095d41905fc947c1c5cd6596962d134728@ec2-34-242-199-141.eu-west-1.compute.amazonaws.com:5432/dc8u9hpggbagcb"
+DB_URI = "postgres://xyusvufljhxfvm:7b38bb605b913143bfe015d76c0451871f7ff25053f7f477ef3db44123fa63ec@ec2-34-247-16-250.eu-west-1.compute.amazonaws.com:5432/dd4sta2driu577"
